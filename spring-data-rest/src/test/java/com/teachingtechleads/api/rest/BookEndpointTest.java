7a41127b26c2fcfd3c57a41426dfcb41f6b420a3
Seth Kellas <sjkellas@gmail.com>
2020-01-28 13:32:45 -0500

    Add test for POST endpoint

--- spring-data-rest/src/test/java/com/teachingtechleads/api/rest/BookEndpointTest.java
+++ spring-data-rest/src/test/java/com/teachingtechleads/api/rest/BookEndpointTest.java
@@ -3,10 +3,14 @@ package com.teachingtechleads.api.rest;
 import static java.lang.String.format;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +19,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.teachingtechleads.data.object.Book;
 import com.teachingtechleads.data.repository.BookRepository;
 
@@ -29,6 +33,27 @@ public class BookEndpointTest {
 
     @Autowired
     private BookRepository bookRepository;
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Test
+    public void shouldCreateOnPost() throws Exception {
+        // Given
+        final Book book = Book.builder().author("Seth Kellas").title("Teaching Tech Leads").build();
+
+        // When
+        final MvcResult response = mvc
+                .perform(post("/books").accept(APPLICATION_JSON).content(mapper.writeValueAsBytes(book)))
+                .andExpect(status().is(CREATED.value())).andReturn();
+
+        // Then
+        final String locationUrl = response.getResponse().getHeader("Location");
+        final Long id = Long.valueOf(locationUrl.substring(locationUrl.lastIndexOf("/") + 1));
+        final Book persistedBook = bookRepository.findById(id).get();
+
+        assertThat(persistedBook.getAuthor(), Matchers.equalTo(book.getAuthor()));
+        assertThat(persistedBook.getTitle(), Matchers.equalTo(book.getTitle()));
+    }
 
     @Test
     public void shouldGetById() throws Exception {
@@ -37,8 +62,7 @@ public class BookEndpointTest {
                 .save(Book.builder().author("Seth Kellas").title("Teaching Tech Leads").build());
 
         // When
-        final MvcResult response = mvc
-                .perform(MockMvcRequestBuilders.get(format("/books/%d", book.getId())).accept(APPLICATION_JSON))
+        final MvcResult response = mvc.perform(get(format("/books/%d", book.getId())).accept(APPLICATION_JSON))
                 .andExpect(status().is(OK.value())).andReturn();
 
         // Then
