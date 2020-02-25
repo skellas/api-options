b5c6c171cd41bb35c036fa982957f53364b6b895
skellas <sjkellas@gmail.com>
2020-02-24 20:05:48 -0500

    Revert "Add in constructor"... cause it's wrong.
    This reverts commit 741e27c3e05858942d20eaf698727484df22a2ee.

--- core-data/src/main/java/com/teachingtechleads/data/object/Book.java
+++ core-data/src/main/java/com/teachingtechleads/data/object/Book.java
@@ -9,11 +9,13 @@ import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Value;
 
 @Entity
 @Value
+@NoArgsConstructor(force = true)
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class Book {
